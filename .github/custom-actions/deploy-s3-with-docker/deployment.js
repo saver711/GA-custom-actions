3bc3f45cbdee1ffe98d796042f9d907427c84965
Ahmed Hassan <ahwork711@gmail.com>
2025-08-02 15:45:22 +0300

    Fix: TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be of type string or an instance of Buffer or URL. Received undefined- attempt 1

--- .github/custom-actions/deploy-s3-with-docker/deployment.js
+++ .github/custom-actions/deploy-s3-with-docker/deployment.js
@@ -4,57 +4,76 @@ const fs = require("fs")
 const mime = require("mime-types")
 
 function run() {
-  // In the action.yml file, we have defined the inputs, Github will automatically
-  // pass the inputs as environment variables like this: INPUT_<INPUT_NAME all in uppercase>
-  const bucket = process.env.INPUT_S3_BUCKET
-  const bucketRegion = process.env.INPUT_AWS_REGION
-  const distFolder = process.env.INPUT_DIST_FOLDER
+  try {
+    // Get inputs from environment variables
+    const bucket = process.env.INPUT_S3_BUCKET
+    const bucketRegion = process.env.INPUT_AWS_REGION || "us-east-1"
+    const distFolder = process.env.INPUT_DIST_FOLDER || "dist"
 
-  const configuration = {
-    region: bucketRegion
-  }
-
-  const s3Client = new AWS.S3(configuration)
-
-  function walkSync(dir, fileList = []) {
-    const files = fs.readdirSync(dir)
-    files.forEach(file => {
-      const filePath = path.join(dir, file)
-      if (fs.statSync(filePath).isDirectory()) {
-        walkSync(filePath, fileList)
-      } else {
-        fileList.push(filePath)
-      }
-    })
-    return fileList
-  }
-
-  const files = walkSync(distFolder)
-
-  const uploadPromises = files.map(filePath => {
-    const key = filePath.replace(distFolder + path.sep, "").replace(/\\/g, "/")
-    const params = {
-      Bucket: bucket,
-      Key: key,
-      Body: fs.createReadStream(filePath),
-      ContentType: mime.lookup(filePath) || "application/octet-stream"
+    if (!bucket) {
+      throw new Error("S3 bucket name is required (INPUT_S3_BUCKET)")
     }
 
-    return s3Client.upload(params).promise()
-  })
+    // Verify dist folder exists
+    if (!fs.existsSync(distFolder)) {
+      throw new Error(`Dist folder not found: ${distFolder}`)
+    }
 
-  Promise.all(uploadPromises)
-    .then(() => {
-      const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
-      fs.appendFileSync(
-        process.env.GITHUB_OUTPUT,
-        `website-url=${websiteUrl}\n`
-      )
+    const configuration = {
+      region: bucketRegion
+    }
+
+    const s3Client = new AWS.S3(configuration)
+
+    function walkSync(dir, fileList = []) {
+      const files = fs.readdirSync(dir)
+      files.forEach(file => {
+        const filePath = path.join(dir, file)
+        if (fs.statSync(filePath).isDirectory()) {
+          walkSync(filePath, fileList)
+        } else {
+          fileList.push(filePath)
+        }
+      })
+      return fileList
+    }
+
+    const files = walkSync(distFolder)
+
+    const uploadPromises = files.map(filePath => {
+      const key = filePath
+        .replace(distFolder + path.sep, "")
+        .replace(/\\/g, "/")
+      const params = {
+        Bucket: bucket,
+        Key: key,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath) || "application/octet-stream"
+      }
+      return s3Client.upload(params).promise()
     })
-    .catch(error => {
-      console.error("Error uploading files:", error)
-      process.exit(1)
-    })
+
+    Promise.all(uploadPromises)
+      .then(() => {
+        const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+        // Write output in GitHub Actions format
+        if (process.env.GITHUB_OUTPUT) {
+          fs.appendFileSync(
+            process.env.GITHUB_OUTPUT,
+            `website-url=${websiteUrl}\n`
+          )
+        }
+        console.log("Deployment successful!")
+        console.log(`Website URL: ${websiteUrl}`)
+      })
+      .catch(error => {
+        console.error("Error uploading files:", error)
+        process.exit(1)
+      })
+  } catch (error) {
+    console.error(error.message)
+    process.exit(1)
+  }
 }
 
 run()
