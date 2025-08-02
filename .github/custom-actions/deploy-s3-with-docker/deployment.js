43e7a790346b99545bb224d498c6142ca64502ed
Ahmed Hassan <ahwork711@gmail.com>
2025-08-02 15:54:43 +0300

    back to using @actions/core with docker action

--- .github/custom-actions/deploy-s3-with-docker/deployment.js
+++ .github/custom-actions/deploy-s3-with-docker/deployment.js
@@ -1,79 +1,16 @@
-const AWS = require("aws-sdk")
-const path = require("path")
-const fs = require("fs")
-const mime = require("mime-types")
-
+const core = require("@actions/core")
+const exec = require("@actions/exec")
 function run() {
-  try {
-    // Get inputs from environment variables
-    const bucket = process.env.INPUT_S3_BUCKET
-    const bucketRegion = process.env.INPUT_AWS_REGION || "us-east-1"
-    const distFolder = process.env.INPUT_DIST_FOLDER || "dist"
+  // 1) Get the input parameters
+  const bucketName = core.getInput("s3-bucket", { required: true })
+  const awsRegion = core.getInput("aws-region", { required: true })
+  const distFolder = core.getInput("dist-folder", { required: true })
 
-    if (!bucket) {
-      throw new Error("S3 bucket name is required (INPUT_S3_BUCKET)")
-    }
+  // 2) Upload the files to S3
+  const uploadCommand = `aws s3 sync ${distFolder} s3://${bucketName} --region ${awsRegion}`
+  exec.exec(uploadCommand)
 
-    // Verify dist folder exists
-    if (!fs.existsSync(distFolder)) {
-      throw new Error(`Dist folder not found: ${distFolder}`)
-    }
-
-    const configuration = {
-      region: bucketRegion
-    }
-
-    const s3Client = new AWS.S3(configuration)
-
-    function walkSync(dir, fileList = []) {
-      const files = fs.readdirSync(dir)
-      files.forEach(file => {
-        const filePath = path.join(dir, file)
-        if (fs.statSync(filePath).isDirectory()) {
-          walkSync(filePath, fileList)
-        } else {
-          fileList.push(filePath)
-        }
-      })
-      return fileList
-    }
-
-    const files = walkSync(distFolder)
-
-    const uploadPromises = files.map(filePath => {
-      const key = filePath
-        .replace(distFolder + path.sep, "")
-        .replace(/\\/g, "/")
-      const params = {
-        Bucket: bucket,
-        Key: key,
-        Body: fs.createReadStream(filePath),
-        ContentType: mime.lookup(filePath) || "application/octet-stream"
-      }
-      return s3Client.upload(params).promise()
-    })
-
-    Promise.all(uploadPromises)
-      .then(() => {
-        const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
-        // Write output in GitHub Actions format
-        if (process.env.GITHUB_OUTPUT) {
-          fs.appendFileSync(
-            process.env.GITHUB_OUTPUT,
-            `website-url=${websiteUrl}\n`
-          )
-        }
-        console.log("Deployment successful!")
-        console.log(`Website URL: ${websiteUrl}`)
-      })
-      .catch(error => {
-        console.error("Error uploading files:", error)
-        process.exit(1)
-      })
-  } catch (error) {
-    console.error(error.message)
-    process.exit(1)
-  }
+  const websiteUrl = `http://${bucketName}.s3-website-${awsRegion}.amazonaws.com`
+  core.setOutput("website-url", websiteUrl)
 }
-
 run()
